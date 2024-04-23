154419480a827d3bf51748cfafe1931dacec821f
hyunseo-k <jelly9999@g.skku.edu>
2024-04-23 14:02:13 +0900

    feat: Dummy data for offer test

---
+++ frontend/src/pages/Offer/dummy.js
@@ -0,0 +1,39 @@
+// dummy.js
+export const dummy = [
+  {
+    id: 1,
+    title: 'Offer 1',
+    description: 'This is a description for Offer 1',
+    status: 'Incoming', // Incoming, Outgoing, or Matched
+  },
+  {
+    id: 2,
+    title: 'Offer 2',
+    description: 'This is a description for Offer 2',
+    status: 'Outgoing', // Incoming, Outgoing, or Matched
+  },
+  {
+    id: 3,
+    title: 'Offer 3',
+    description: 'This is a description for Offer 3',
+    status: 'Matched', // Incoming, Outgoing, or Matched
+  },
+  {
+    id: 4,
+    title: 'Offer 4',
+    description: 'This is a description for Offer 4',
+    status: 'Incoming', // Incoming, Outgoing, or Matched
+  },
+  {
+    id: 5,
+    title: 'Offer 5',
+    description: 'This is a description for Offer 5',
+    status: 'Outgoing', // Incoming, Outgoing, or Matched
+  },
+  {
+    id: 6,
+    title: 'Offer 6',
+    description: 'This is a description for Offer 6',
+    status: 'Matched', // Incoming, Outgoing, or Matched
+  },
+];
